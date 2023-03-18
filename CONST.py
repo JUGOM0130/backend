b5bb85bc29c063c6bc695840da235cd66c6a1b53
JunMac <junnehoomaki@yahoo.co.jp>
2023-03-18 09:10:59 +0900

    bachAddMac

--- CONST.py
+++ CONST.py
@@ -1,12 +1,52 @@
+import sys
 import logging
 
-# ログレベル設定 リリース時はINFO以上に設定する
+# デフォルト設定
 logging.basicConfig(level=logging.DEBUG)
-
-# 定数設定
 CONST = {
     'user': 'admin',
     'pw': '0000',
     'host': '192.168.0.20',
     'db': 'a_system'
 }
+
+if len(sys.argv) > 0:
+    # 本番環境用設定
+    if sys.argv[0] == "PRO":
+        print("本番環境運用")
+        # ログレベル設定 リリース時はINFO以上に設定する
+        logging.basicConfig(level=logging.DEBUG)
+
+        # 定数設定
+        CONST = {
+            'user': 'admin',
+            'pw': '0000',
+            'host': '192.168.0.20',
+            'db': 'a_system'
+        }
+    # 会社Windows
+    elif sys.argv[0] == "WIN":
+        print("Win環境運用")
+        # ログレベル設定 リリース時はINFO以上に設定する
+        logging.basicConfig(level=logging.DEBUG)
+
+        # 定数設定
+        CONST = {
+            'user': 'admin',
+            'pw': '0000',
+            'host': '192.168.0.20',
+            'db': 'a_system'
+        }
+    # 検証環境用
+    else:
+        print("検証環境運用")
+        # ログレベル設定 リリース時はINFO以上に設定する
+        logging.basicConfig(level=logging.DEBUG)
+
+        # 定数設定
+        CONST = {
+            'user': 'admin',
+            'pw': '0000',
+            'host': '192.168.0.20',
+            'db': 'a_system'
+        }
--- main.py
+++ main.py
@@ -37,7 +37,7 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="127.0.0.1",port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 """
 @app.get('/testinsert')
